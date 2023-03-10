dd39058c9f6e49c9e8398c9bee7ac9aa1bd34c51
jiandong <fivezjd@gmai.com>
2023-03-10 18:46:32 +0800

    源码分析

--- mux_test.go
+++ mux_test.go
@@ -1,4 +1,13 @@
 package asynq
 
+import (
+	"io"
+	"sync"
+)
+
+type handleF func(r io.Reader) error
+
 type ServerMux struct {
+	mu      sync.RWMutex       // 控制mu方法的原子性
+	handMap map[string]handleF // 保存各个handle方法
 }
